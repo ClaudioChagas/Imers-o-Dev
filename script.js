34b0c389da6390fea99f80ab1e61a521714173f8
claudio <claudiochagasff@gmail.com>
2021-09-15 11:40:20 -0300

    Calculadora de Notas

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var nome = "claudio"
+var notaDoPrimeiroBimestre = 9.555
+var notaDoSegundoBimestre = 7.873
+var notaDoTerceiroBimestre = 4.546
+var notaDoQuartoBimestre = 2.076
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+
+
+console.log("Bem vindo " + nome)
+alert(notaFixada)
